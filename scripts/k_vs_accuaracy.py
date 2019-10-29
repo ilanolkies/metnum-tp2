c99d01a561a90dcf50b8da45385f0110da44e601
Ilan Olkies <ilanolkies@outlook.com>
2019-10-29 01:36:54 -0300

    Agregando promedio de repeticiones a knn

--- scripts/k_vs_accuaracy.py
+++ scripts/k_vs_accuaracy.py
@@ -11,101 +11,92 @@ import matplotlib.pyplot as plt
 import time
 import argparse
 
-
-def get_instances(df):
+# para un dataset `df`
+# obtiene instancias de train y test
+# crea el modelo de bolsa de palabras
+# y poda los datos desde el `min_df` al `max_df`
+def get_instances(df, max_df=0.90, min_df=0.1):
 	text_train = df[df.type == 'train']["review"]
 	label_train = df[df.type == 'train']["label"]
 
 	text_test = df[df.type == 'test']["review"]
 	label_test = df[df.type == 'test']["label"]
 
-	print("Cantidad de instancias de entrenamiento = {}".format(len(text_train)))
-	print("Cantidad de instancias de test = {}".format(len(text_test)))
-
-	print("Class balance : {} pos {} neg".format(
-		(label_train == 'pos').sum() / label_train.shape[0],
-		(label_train == 'neg').sum() / label_train.shape[0]
-	))
-
-	vectorizer = CountVectorizer(max_df=0.90, min_df=0.1, max_features=5000)
+	vectorizer = CountVectorizer(max_df=max_df, min_df=min_df, max_features=5000)
 
 	vectorizer.fit(text_train)
 
 	X_train, y_train = vectorizer.transform(text_train), (label_train == 'pos').values
 	X_test, y_test = vectorizer.transform(text_test), (label_test == 'pos').values
 
-
-
 	return X_train, y_train, X_test, y_test
 
+def evaluate_knn(k_range, X_train, y_train, X_test, y_test, reps):
+	accs = []
 
+	# kNN
+	for k in k_range:
+		t = 0
+
+		for i in range(reps):
+			clf = KNNClassifier(k)
+			clf.fit(X_train, y_train)
+			y_pred = clf.predict(X_test)
+			t += accuracy_score(y_test, y_pred)
+
+		accs.append(t/reps)
+
+	return accs
 
 def main(args):
-
-
-	#Genero el rango de elementos a utilizar
-	its = np.arange(args.k_Start, args.k_Stop, args.k_Step)
-
-	its = [int(i) for i in its]
-
-	alpha = args.alpha_Start
-
-	'''
-	Obteniendo datasets
-	'''
+	# datasets de train y test
 	df = pd.read_csv("data/imdb_small.csv")
 
 	#recortando data set para pruebas chiquitas
-
 	df = df[:args.elem]
 
-	'''
-	Vectorizacion
-	'''
-	print("vectorizacion")
-
 	X_train, y_train, X_test, y_test = get_instances(df)
-	
-	'''
-	Procesando PCA
-	'''
-	print("PCA")
+
+	alpha = args.alpha_Start
+
+	# rango de k
+	k_range = np.arange(args.k_Start, args.k_Stop, args.k_Step)
+
+	# rango de alpha
+	alpha_range = np.arange(alpha, args.alpha_Stop, args.alpha_Step)
+
+	# resultados de accuaracies para cada alpha
+	results = []
+
 	if (alpha > 0):
-		pca = PCA(alpha)
-		
-		pca.fit(X_train.toarray())
+		# PCA
+		for alpha in alpha_range:
+			pca = PCA(alpha)
+			pca.fit(X_train.toarray())
+			X_train = pca.transform(X_train)
+			X_test = pca.transform(X_test)
 
-		X_train = pca.transform(X_train)
-		#print(X_train)
-		X_test = pca.transform(X_test)
-		#print(X_train)
+			# resultados de accuaracy con cada k
+			accs = evaluate_knn(k_range, X_train, y_train, X_test, y_test, args.reps)
+			results.append(accs)
+	else:
+		# resultados de accuaracy con cada k
+		accs = evaluate_knn(k_range, X_train, y_train, X_test, y_test, args.reps)
+		results.append(accs)
 
+	if (alpha > 0):
+		for i, result in results:
+			plt.plot(k_range, result, label='alpha = {}'.format(alpha_range[i]))
+	else:
+		plt.plot(k_range, results[0], label='sin pca')
 
-	accs = []
-
-
+	plt.xlabel('k')
+	plt.ylabel('accuaracy')
+	plt.legend()
+	plt.savefig('results/k_vs_accuaracy-{}'.format(time.strftime("%Y%m%d-%H%M%S")))
+	plt.show()
 
 	'''
-	El experimento, iterando en k
-	'''
-	print("KNN")
-	for k in its:
-		#print('K: {0}'.format(k))
-		
-		#Entrenamos KNN
-		print("Entrenamiento")
-		clf = KNNClassifier(k)
-		clf.fit(X_train, y_train)
-
-		print("predict")
-		y_pred = clf.predict(X_test)
-		print("accuracy")
-		acc = accuracy_score(y_test, y_pred)
-		accs.append(acc)
-
-	print("KNN finalizado")
-
-
 	fig, ax = plt.subplots()
 	ax.plot(its, accs)
 
@@ -115,61 +106,71 @@ def main(args):
 
 	fig.savefig("results/k_vs_accuaracy_{0}.png".format(time.strftime("%Y%m%d-%H%M%S")))
 	plt.show()
-
-
-
-
-
-
+	'''
 
 def positive_integer(value):
-    ivalue = int(value)
-    if ivalue <= 0:
-        raise argparse.ArgumentTypeError("%s Valor inválido, tiene que ser un numero natural." % value)
-    return ivalue
-
-
+	ivalue = int(value)
+	if ivalue <= 0:
+		raise argparse.ArgumentTypeError("%s Valor inválido, tiene que ser un numero natural." % value)
+	return ivalue
 
 if __name__ == '__main__':
-
-
 	description = 'Acuraccy.'
-	parser = argparse.ArgumentParser(description=description)
-	parser.add_argument('--k_Start',
-						type=positive_integer,
-						default=1,
-						help='Valor minimo de K.')
-	parser.add_argument('--k_Stop',
-						type=positive_integer,
-						default=50,
-						help='Valor maximo de K.')
-	parser.add_argument('--k_Step',
-						type=positive_integer,
-						default=10,
-						help="Diferencia entre cada alpha")
-	parser.add_argument('--alpha_Start',
-						type=positive_integer,
-						default=0,
-						help="Valor minimo de alpha")
-	parser.add_argument('--alpha_Stop',
-						type=positive_integer,
-						default=15,
-						help="Valor maximo Alpha")
-	parser.add_argument('--alpha_Step',
-						type=int,
-						default=21,
-						help="Diferencia entre cada alpha.")
-	parser.add_argument('--elem',
-						type=positive_integer,
-						default=100,
-						help='Cantidad de elementos tomados de la base de datos')
 
+	parser = argparse.ArgumentParser(description=description)
+
+	parser.add_argument(
+		'--k_Start',
+		type=positive_integer,
+		default=1,
+		help='Valor minimo de k'
+	)
+	parser.add_argument(
+		'--k_Stop',
+		type=positive_integer,
+		default=50,
+		help='Valor maximo de k'
+	)
+	parser.add_argument(
+		'--k_Step',
+		type=positive_integer,
+		default=10,
+		help='Salto de k'
+	)
+	parser.add_argument(
+		'--alpha_Start',
+		type=positive_integer,
+		default=0,
+		help='Valor minimo de alpha'
+	)
+	parser.add_argument(
+		'--alpha_Stop',
+		type=positive_integer,
+		default=50,
+		help='Valor maximo de alpha'
+	)
+	parser.add_argument(
+		'--alpha_Step',
+		type=positive_integer,
+		default=10,
+		help='Salto de alpha'
+	)
+	parser.add_argument(
+		'--elem',
+		type=positive_integer,
+		default=1000,
+		help='Poda bruta de comentarios'
+	)
+	parser.add_argument(
+		'--reps',
+		type=positive_integer,
+		default=10,
+		help='Repeticiones de kNN'
+	)
 	args = parser.parse_args()
 
-	assert args.k_Start > 0 
+	assert args.k_Start > 0
 	assert args.k_Stop < args.elem
 	assert args.k_Start <= args.k_Stop
 
-
 	main(args)
-
